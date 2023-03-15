2a10dcd204007952a036cbfbd473b240e160cb27
Roberto Ramirez <robert.624@hotmail.com>
2023-03-15 09:59:39 -0500

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routerApi from './routes/index.js';
 import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler.js';
 
@@ -6,6 +7,19 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://localhost:3000','http://localhost:5500', 'https://www.myapp.com', 'http://127.0.0.1:5500/']
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null, true)
+    }
+    else {
+      callback(new Error('Forbidden'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
